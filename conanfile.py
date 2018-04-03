13e59d79bdbb2ae1f542194cc9fd449f03467d0c
dimitri <dimi309@users.noreply.github.com>
2018-04-03 05:12:53 +0200

    Linking with m also when using gcc v4.9 in shared build.

--- conanfile.py
+++ conanfile.py
@@ -73,5 +73,8 @@ class VorbisConan(ConanFile):
         else:
             self.cpp_info.libs = ['vorbisfile', 'vorbisenc', 'vorbis']
 
-        if self.settings.os == "Linux" and not self.options.shared:
+        if self.settings.os == "Linux" and (not self.options.shared or\
+                                            (self.settings.compiler=="gcc" and\
+                                             self.options.compiler.version=="4.9")):
             self.cpp_info.libs.append("m")
+
