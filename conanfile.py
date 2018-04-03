61333e5e82d89f8aa75a0381160462bd004e0557
dimitri <dimi309@users.noreply.github.com>
2018-04-03 05:24:53 +0200

    Prepending m

--- conanfile.py
+++ conanfile.py
@@ -71,8 +71,6 @@ class VorbisConan(ConanFile):
         else:
             self.cpp_info.libs = ['vorbisfile', 'vorbisenc', 'vorbis']
 
-        if self.settings.os == "Linux" and (not self.options.shared or\
-                                            (self.settings.compiler=="gcc" and\
-                                             self.settings.compiler.version=="4.9")):
-            self.cpp_info.libs.append("m")
+        if self.settings.os == "Linux" and not self.options.shared:
+            self.cpp_info.libs.insert(0, "m")
 
