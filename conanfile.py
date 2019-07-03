0e92e5eeb9a5316e3e72007617b2828914a9af40
Anonymous Maarten <anonymous.maarten@gmail.com>
2019-07-03 23:51:27 +0200

    Run bincrafters-conventions
    commit by conan-repo-actions

--- conanfile.py
+++ conanfile.py
@@ -14,10 +14,10 @@ class VorbisConan(ConanFile):
     license = "BSD"
     exports = ["LICENSE.md"]
     exports_sources = ["CMakeLists.txt", "FindVORBIS.cmake"]
-    source_subfolder = "source_subfolder"
+    _source_subfolder = "source_subfolder"
     settings = "os", "arch", "build_type", "compiler"
     options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = "shared=False", "fPIC=True"
+    default_options = {'shared': False, 'fPIC': True}
     requires = "ogg/1.3.3@bincrafters/stable"
     generators = "cmake"
 
@@ -30,9 +30,9 @@ class VorbisConan(ConanFile):
 
     def source(self):
         tools.get("https://github.com/xiph/vorbis/archive/v%s.tar.gz" % self.version)
-        os.rename("vorbis-%s" % self.version, self.source_subfolder)
+        os.rename("vorbis-%s" % self.version, self._source_subfolder)
         if self.settings.os == 'Windows':
-            with tools.chdir(self.source_subfolder):
+            with tools.chdir(self._source_subfolder):
                 tools.replace_in_file('vorbis.pc.in', 'Libs.private: -lm', 'Libs.private:')
 
     def build(self):
@@ -49,9 +49,9 @@ class VorbisConan(ConanFile):
 
     def package(self):
         self.copy("FindVORBIS.cmake", ".", ".")
-        self.copy("include/vorbis/*", ".", "%s" % self.source_subfolder, keep_path=True)
-        self.copy("%s/copying*" % self.source_subfolder, dst="licenses",  ignore_case=True, keep_path=False)
-        self.copy('*.pc', src=self.source_subfolder, dst=os.path.join('lib', 'pkgconfig'), keep_path=False)
+        self.copy("include/vorbis/*", ".", "%s" % self._source_subfolder, keep_path=True)
+        self.copy("%s/copying*" % self._source_subfolder, dst="licenses",  ignore_case=True, keep_path=False)
+        self.copy('*.pc', src=self._source_subfolder, dst=os.path.join('lib', 'pkgconfig'), keep_path=False)
 
         if self.settings.compiler == "Visual Studio":
             if self.options.shared:
