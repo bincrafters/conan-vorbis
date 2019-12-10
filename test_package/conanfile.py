bincrafters/conan-vorbis

commit cb6c16c99b5d98af9d5075f5486253abf8b75b41

Update conventions

test_package/conanfile.py
@@ -14,10 +14,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def imports(self):
-        self.copy("*.dll", dst="bin", src="bin")
-        self.copy("*.dylib*", dst="bin", src="lib")
-        self.copy('*.so*', dst='bin', src='lib')
-
         test_package_dir = os.path.dirname(os.path.abspath(__file__))
         self.copy("sample.wav", src=test_package_dir, dst="bin")
 
@@ -34,8 +30,8 @@ class TestPackageConan(ConanFile):
                         )
                     except subprocess.CalledProcessError as e:
                         self.output.error(
-                            "Test Error!!! cmd: %s return code: %s output: %s" % (e.cmd, e.returncode, e.output)
+                            "Test Error! cmd: %s return code: %s output: %s" % (e.cmd, e.returncode, e.output)
                         )
                         sys.exit(e.returncode)
                     else:
-                        self.output.success("Test OK!!!")
+                        self.output.success("Test OK!")

conanfile.py
@@ -8,7 +8,6 @@ class VorbisConan(ConanFile):
     description = "The VORBIS audio codec library"
     topics = ("conan", "vorbis", "audio", "codec")
     url = "http://github.com/bincrafters/conan-vorbis"
-    author = "Bincrafters <bincrafters@gmail.com>"
     homepage = "https://xiph.org/vorbis/"
     license = "BSD-3-Clause"
     exports = ["LICENSE.md"]
@@ -17,7 +16,7 @@ class VorbisConan(ConanFile):
     settings = "os", "arch", "build_type", "compiler"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {'shared': False, 'fPIC': True}
-    requires = "ogg/1.3.3@bincrafters/stable"
+    requires = "ogg/1.3.4"
     generators = "cmake"
 
     def config_options(self):
@@ -29,9 +28,9 @@ class VorbisConan(ConanFile):
         del self.settings.compiler.cppstd
 
     def source(self):
-        url = 'https://github.com/xiph/vorbis/archive/v%s.tar.gz' % self.version
-        tools.get(url, sha256='43fc4bc34f13da15b8acfa72fd594678e214d1cab35fc51d3a54969a725464eb')
-        os.rename("vorbis-%s" % self.version, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
         if self.settings.os == 'Windows':
             with tools.chdir(self._source_subfolder):
                 tools.replace_in_file('vorbis.pc.in', 'Libs.private: -lm', 'Libs.private:')
@@ -70,8 +69,6 @@ class VorbisConan(ConanFile):
                 self.copy(pattern="*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-
         self.cpp_info.libs = ['vorbisfile', 'vorbisenc', 'vorbis']
-
         if self.settings.os == "Linux":
             self.cpp_info.libs.append("m")

CMakeLists.txt
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 2.8.11)
+cmake_minimum_required(VERSION 2.8.12)
 project(cmake_wrapper)
 
 include(conanbuildinfo.cmake)

appveyor.yml
@@ -1,27 +1,14 @@
-build: false
-
 environment:
-    PYTHON_HOME: "C:\\Python37"
+  PYTHON_HOME: "C:\\Python37"
 
-    matrix:
-        - APPVEYOR_BUILD_WORKER_IMAGE: Visual Studio 2015
-          CONAN_VISUAL_VERSIONS: 14
-          CONAN_BUILD_TYPES: Release
-        - APPVEYOR_BUILD_WORKER_IMAGE: Visual Studio 2015
-          CONAN_VISUAL_VERSIONS: 14
-          CONAN_BUILD_TYPES: Debug
-        - APPVEYOR_BUILD_WORKER_IMAGE: Visual Studio 2017
-          CONAN_VISUAL_VERSIONS: 15
-          CONAN_BUILD_TYPES: Release
-        - APPVEYOR_BUILD_WORKER_IMAGE: Visual Studio 2017
-          CONAN_VISUAL_VERSIONS: 15
-          CONAN_BUILD_TYPES: Debug
-        - APPVEYOR_BUILD_WORKER_IMAGE: Visual Studio 2019
-          CONAN_VISUAL_VERSIONS: 16
-          CONAN_BUILD_TYPES: Release
-        - APPVEYOR_BUILD_WORKER_IMAGE: Visual Studio 2019
-          CONAN_VISUAL_VERSIONS: 16
-          CONAN_BUILD_TYPES: Debug
+  matrix:
+      - APPVEYOR_BUILD_WORKER_IMAGE: Visual Studio 2015
+        CONAN_VISUAL_VERSIONS: 14
+        CONAN_BUILD_TYPES: Debug
+      - APPVEYOR_BUILD_WORKER_IMAGE: Visual Studio 2017
+        CONAN_VISUAL_VERSIONS: 15
+      - APPVEYOR_BUILD_WORKER_IMAGE: Visual Studio 2019
+        CONAN_VISUAL_VERSIONS: 16
 
 install:
   - set PATH=%PYTHON_HOME%;%PYTHON_HOME%/Scripts/;%PATH%
@@ -29,5 +16,5 @@ install:
   - pip.exe install conan_package_tools bincrafters_package_tools
   - conan user # It creates the conan data directory
 
-test_script:
+build_script:
   - python build.py

build.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 
 from bincrafters import build_template_default
 

conandata.yml
@@ -1,0 +1,4 @@
+sources:
+  "1.3.6":
+    url: "https://github.com/xiph/vorbis/archive/v1.3.6.tar.gz"
+    sha256: "43fc4bc34f13da15b8acfa72fd594678e214d1cab35fc51d3a54969a725464eb"

test_package/CMakeLists.txt
@@ -1,13 +1,11 @@
+cmake_minimum_required(VERSION 2.8.12)
 project(test_package)
-cmake_minimum_required(VERSION 2.8.11)
 
 set(CMAKE_VERBOSE_MAKEFILE TRUE)
 
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()
 
-file(GLOB SOURCE_FILES *.cpp)
-
-add_executable(${PROJECT_NAME} ${SOURCE_FILES})
+add_executable(${PROJECT_NAME} test_package.cpp)
 target_link_libraries(${PROJECT_NAME} ${CONAN_LIBS})
 set_property(TARGET ${PROJECT_NAME} PROPERTY CXX_STANDARD 11)

commit 41e80977e6812cef40f2672b318d2d131a230507

fix windows and macos test
they were broken by cb6c16c99b5d98af9d5075f5486253abf8b75b41

test_package/conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, CMake, tools, RunEnvironment
 import os
 import subprocess
 import sys
@@ -21,17 +21,19 @@ class TestPackageConan(ConanFile):
         with tools.chdir("bin"):
             with open("sample.wav", "rb") as input_file:
                 with open("sample.ogg", "wb") as output_file:
-                    try:
-                        subprocess.check_call(
-                            [".%stest_package" % os.sep],
-                            stdin=input_file,
-                            stdout=output_file,
-                            stderr=subprocess.STDOUT
-                        )
-                    except subprocess.CalledProcessError as e:
-                        self.output.error(
-                            "Test Error! cmd: %s return code: %s output: %s" % (e.cmd, e.returncode, e.output)
-                        )
-                        sys.exit(e.returncode)
-                    else:
-                        self.output.success("Test OK!")
+                    env_build = RunEnvironment(self)
+                    with tools.environment_append(env_build.vars):
+                        try:
+                            subprocess.check_call(
+                                [".%stest_package" % os.sep],
+                                stdin=input_file,
+                                stdout=output_file,
+                                stderr=subprocess.STDOUT
+                            )
+                        except subprocess.CalledProcessError as e:
+                            self.output.error(
+                                "Test Error! cmd: %s return code: %s output: %s" % (e.cmd, e.returncode, e.output)
+                            )
+                            sys.exit(e.returncode)
+                        else:
+                            self.output.success("Test OK!")